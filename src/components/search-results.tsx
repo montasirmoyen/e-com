647c838a7d90a1c241d0bbde087a1371ffb73fb2
montasirmoyen <montasirmoyen@gmail.com>
2025-10-04 19:20:26 -0400

    Add view item to result

--- src/components/search-results.tsx
+++ src/components/search-results.tsx
@@ -43,6 +43,12 @@ export default function SearchResults({ query }: Props) {
               </div>
               <h3 className="text-md text-gray-700 font-bold mb-1 text-center">{item.title}</h3>
               <p className="text-gray-700 mb-2">${item.price}</p>
+              <button
+                style={{ backgroundColor: "#d8043cff" }}
+                className="text-white px-4 py-2 rounded mt-auto"
+              >
+                View Item
+              </button>
             </div>
           ))}
         </div>
