eba28ab2e60bc667b9873450b5c2884a788f30ce
montasirmoyen <montasirmoyen@gmail.com>
2025-10-04 19:19:39 -0400

    Remove dynamic search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,23 @@
 "use client";
 
 import Navbar from "../components/nav-bar";
-import { useState } from "react";
-import SearchResults from "../components/search-results";
 import Recommendations from "../components/recommendations";
 
 export default function Home() {
-  const [query, setQuery] = useState("");
-
   return (
-    <Navbar onSearch={(q) => setQuery(q)}>
+    <Navbar>
       <div className="min-h-screen bg-gray-100">
-        {query ? (
-          <SearchResults query={query} />
-        ) : (
-          <>
-            <Recommendations
-              title="Tech Recommendations"
-              subtitle="Tech products you may like"
-              ids={[9, 10, 11, 12, 13]}
-            />
+        <Recommendations
+          title="Tech Recommendations"
+          subtitle="Tech products you may like"
+          ids={[9, 10, 11, 12, 13]}
+        />
 
-            <Recommendations
-              title="Fashion Recommendations"
-              subtitle="Fashion clothing you may like"
-              ids={[1, 2, 3, 15, 5]}
-            />
-          </>
-        )}
+        <Recommendations
+          title="Fashion Recommendations"
+          subtitle="Fashion clothing you may like"
+          ids={[1, 2, 3, 15, 5]}
+        />
       </div>
     </Navbar>
   );
